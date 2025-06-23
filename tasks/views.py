1a297f2b914c2cfbd26cc3b7ded984de667a3630
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-23 22:30:51 +0530

    created views for login

--- tasks/views.py
+++ tasks/views.py
@@ -19,3 +19,16 @@ def register_user(request):
         return Response({'token': token.key})
     
     return Response(serializer.errors)
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    
+    if user:
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    
+    return Response({'error': 'Invalid credentials'})
+
