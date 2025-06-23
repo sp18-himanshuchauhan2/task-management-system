dffa9c9e801e8fa7914ce1d534d936da930abab7
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-23 22:30:00 +0530

    created views for register

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from django.contib.auth import authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+    serializer = RegisterSerializer(data = data)
+    
+    if serializer.is_valid():
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user = user)
+        return Response({'token': token.key})
+    
+    return Response(serializer.errors)
