42cd190ee1b8616c199eb7b827c8537347ebd327
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-23 22:04:57 +0530

    update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', # added manually
     'tasks', # added manually
-    'rest_framework.authtoken', # added manually
+    'rest_framework.authtoken', # for token authentication
 
 ]
 
