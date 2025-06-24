17bd02de35e922f6342df81c48fb0a0682197ac1
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-24 16:36:12 +0530

    added urls for time-line

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register_user, login_user, TaskListCreateView, TaskDetailView
+from .views import register_user, login_user, TaskListCreateView, TaskDetailView, TaskTimelineView
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+      path('tasks/timeline/', TaskTimelineView.as_view(), name='task-timeline'),
 ]
