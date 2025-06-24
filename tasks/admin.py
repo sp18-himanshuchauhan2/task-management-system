a1af3bd3ab5e55f78b363f3c40827fe0460b58bd
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-24 11:14:34 +0530

    added tasks in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list = ('title', 'assigned_to', 'status', 'priority', 'due_date', 'created_at')
+    search = ('title', 'desc')
+    filetr = ('status', 'priority', 'due_date')
