d3fc4eb1ddd4896a18775d097aad9f1bce8ba9ab
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-24 18:06:12 +0530

    added token views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import register_user, login_user, TaskListCreateView, TaskDetailView, TaskTimelineView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-      path('tasks/timeline/', TaskTimelineView.as_view(), name='task-timeline'),
+    path('tasks/timeline/', TaskTimelineView.as_view(), name='task-timeline'),
+    # JWT Auth
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
