a6256b497840be3c2e9cbb89bb50791f86038f08
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-25 14:34:09 +0530

    registered models on admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Status, Priority
 
 # Register your models here.
 
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list = ('title', 'assigned_to', 'status', 'priority', 'due_date', 'created_at')
-    search = ('title', 'desc')
-    filetr = ('status', 'priority', 'due_date')
+admin.site.register(Task)
+admin.site.register(Status)
+admin.site.register(Priority)
+
+# @admin.register(Task)
+# class TaskAdmin(admin.ModelAdmin):
+#     list = ('title', 'assigned_to', 'status', 'priority', 'due_date', 'created_at')
+#     search = ('title', 'desc')
+#     filetr = ('status', 'priority', 'due_date')
