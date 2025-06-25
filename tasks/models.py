1a4132ba9e5817112398171ca36182abe74c8e39
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-25 11:52:41 +0530

    created 2 more models

--- tasks/models.py
+++ tasks/models.py
@@ -3,24 +3,25 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Status(models.Model):
+    st = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.st
+
+
+class Priority(models.Model):
+    pr = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.pr
+
 class Task(models.Model):
-    STATUS = [
-        ('todo', 'To Do'),
-        ('inprogress', 'In Progress'),
-        ('done', 'Done'),
-    ]
-
-    PRIORITY = [
-        ('low', 'Low'),
-        ('medium', 'Medium'),
-        ('high', 'High'),
-    ]
-
     title = models.CharField(max_length=255)
     desc = models.TextField(blank=True)
     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
-    status = models.CharField(max_length=20, choices=STATUS, default='todo')
-    priority = models.CharField(max_length=10, choices=PRIORITY, default='medium')
+    status = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True)
+    priority = models.ForeignKey(Priority, on_delete=models.SET_NULL, null=True)
     due_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
 
