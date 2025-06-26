02026ad8932707c244db4438d749aba7e8b2aded
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-26 11:30:27 +0530

    migrated the data from sqlite3 to postgres

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tms",
+        "USER": "postgres",
+        "PASSWORD": "2021323",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
