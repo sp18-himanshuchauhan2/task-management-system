391b649521e46641cbbf60f20c7bcace19fd624c
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-27 17:14:42 +0530

    added settings & static in url-patterns

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -41,3 +41,9 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
